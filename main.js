e1f12dbcd1dc18214399c797a28118c485e9c752
koneshna <guditisn@gmail.com>
2023-06-29 12:47:06 -0400

    added mouseover background color shift

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let gridSize=4
+let gridSize=10
 const container = document.querySelector('.container');
 
 const createGrid = (dimensions) => {
@@ -10,6 +10,8 @@ for ( let i=0; i < dimensions; i++){
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
             row.appendChild(gridBox)
+            gridBox.addEventListener('mouseenter', ()=>
+            {gridBox.style.backgroundColor = 'white'})
         }
         container.appendChild(row)
     }
