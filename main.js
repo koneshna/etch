36833116a6501ff970821711d017995268264951
koneshna <guditisn@gmail.com>
2023-07-01 00:20:10 -0400

    scaling grid size

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
-let gridSize=10
+let gridSize=4
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('button');
 
 const createGrid = (dimensions) => {
-for ( let i=0; i < dimensions; i++){
+    for ( let i=0; i < dimensions; i++){
         const row = document.createElement('div')
         row.classList.add('grid-row')
 
         for(let j =0;j<dimensions;j++){
+            const widthAndHeight = 960 / gridSize
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
+            gridBox.addEventListener('mouseenter', ()=>{
+                gridBox.style.backgroundColor = 'white'
+            })
             row.appendChild(gridBox)
-            gridBox.addEventListener('mouseenter', ()=>
-            {gridBox.style.backgroundColor = 'black'})
         }
+
         container.appendChild(row)
+        
     }
     
 }
-resetButton.addEventListener('click', () => {
-    let userSize = userSize = Number(prompt('what dimensions do you want for the grid'))
-    while (userSize > 100){
-        userSize = Number(prompt('pick number under 100'))
-       
-    }
-    createGrid(userSize)
-}
-)
+createGrid(gridSize)
+//resetButton.addEventListener('click', () => {
+//let userSize = userSize = Number(prompt('what dimensions do you want for the grid'))
+//while (userSize > 100){
+//        userSize = Number(prompt('pick number under 100'))
+//       
+//    }
+//    createGrid(userSize)
+//}
+//)
